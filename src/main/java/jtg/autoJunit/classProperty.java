f73b0f563b6d944ed4cd91c9a9133fd50a253514
怿 安 <a542674348@gmail.com>
2024-01-03 23:21:42 +0800

    ftl

--- src/main/java/jtg/autoJunit/classProperty.java
+++ src/main/java/jtg/autoJunit/classProperty.java
@@ -1,8 +1,8 @@
 package jtg.autoJunit;
 
 import java.lang.reflect.Method;
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.lang.reflect.Type;
+import java.util.*;
 
 //存储一个class的信息
 public class classProperty {
@@ -14,7 +14,9 @@ public class classProperty {
 
     private final Set<String> mtdsName=new LinkedHashSet<>();
     private final Set<String> imports=new LinkedHashSet<>();
+    private final Map<String,Set<String>>mtdTcs=new HashMap<>();
 
+    private final Set<IMethod> meths=new LinkedHashSet<>();
     public classProperty(String cname){
         className=cname;
 
@@ -31,17 +33,50 @@ public class classProperty {
     public Set<Method>getMethods(){
         return methods;
     }
-
-    public Set<String>getMtdsName(){return mtdsName;}
+    public Set<String>getmtdsName(){return mtdsName;}
     public Set<String>getImports(){
         return imports;
     }
 
+    public Set<IMethod> getMeths() {
+        return meths;
+    }
+
+    public Map<String,Set<String>>getMtdTcs(){
+        return mtdTcs;
+    }
+
+
     public void addMethod(Method method){
 
         methods.add(method);
-        mtdsName.add(method.getName());
+        String mName=method.getName();
+        mtdsName.add(mName);
+
+        Set<String> tempstr=new LinkedHashSet<>();
+        tempstr.add("test1");
+        tempstr.add("test2");
+        tempstr.add("test3");
+        mtdTcs.put(mName,tempstr);
+        //IMethod
+        String tmp=method.getReturnType().toString();
+        String[] tb=tmp.split("\\.");           //处理returnType
+
+        IMethod tempImtd=new IMethod(method,mName,tb[tb.length-1]);
+
+        Type[] paraTypes = method.getParameterTypes();
+
+        for(Type para : paraTypes){
+            String str=para.getTypeName();
+
+            String[] buff=str.split("\\.");
+
+            tempImtd.AddParam(buff[buff.length-1]);
+        }
+        meths.add(tempImtd);
+
     }
+
 
     public void addImport(String str){
         imports.add(str);
--- src/main/java/jtg/autoJunit/test.ftl
+++ src/main/java/jtg/autoJunit/test.ftl
@@ -2,17 +2,25 @@ package ${pkg};
 
 import org.junit.jupiter.api.Test;
 
-<#list imports as impt>
 
-    import ${impt};
-</#list>
+<#assign mapmtdTcs=mtdTcs/>      <#-- 声明method到Tcs的map -->
+
+
+
 
 public class ${className}{
 
-<#list mtdsName as mtd>
-    @Test
-    void ${mtd}(){
+<#list meths as meth>
 
+<#assign tcslist=mapmtdTcs[meth.name]/>
+
+    @Test
+    void ${meth.name}(){
+
+    <#list tcslist as tc>
+
+    <#if meth.returnType != "void" >${meth.returnType} temp = </#if>${meth.name}(<#list meth.paramTypes as para>${para},</#list>${tc});
+    </#list>
     }
 </#list>
 
